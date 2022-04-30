da9500327e734b202316d5204a93da3fffe986e5
muhammad-jiku <muhammadjiku364@gmail.com>
2022-04-30 22:58:59 +0600

    description section added to the homepage

--- src/Pages/Home/Description/Description.js
+++ src/Pages/Home/Description/Description.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
 const Description = () => {
-  return <div>This is description box</div>;
+  return (
+    <div className="d-flex">
+      <div>
+        <h2>WELCOME TO YOUR NEW WEBSITE</h2>
+        <p>
+          Oer thy caught sent a upon peering, that with merely was head. Plume
+          seeing grew tufted many radiant its, rapping said purple while morrow.
+          The.
+        </p>
+        <p>
+          Oer thy caught sent a upon peering, that with merely was head. Plume
+          seeing grew tufted many radiant its, rapping said purple while morrow.
+          The.
+        </p>
+      </div>
+      <div>
+        <h2>SEARCH OUR INVENTORY</h2>
+        <input type="text" placeholder="Search" className="my-2" />
+        <br />
+        <input type="button" value="Search" className="me-2" />
+        <input type="button" value="Reset" />
+      </div>
+    </div>
+  );
 };
 
 export default Description;
