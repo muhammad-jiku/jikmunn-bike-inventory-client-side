d6cf9530f7c92c495822a1d599f7af0ea5301342
muhammad-jiku <muhammadjiku364@gmail.com>
2022-05-02 22:58:14 +0600

    login page edited

--- src/Pages/Authentication/LogIn/LogIn.js
+++ src/Pages/Authentication/LogIn/LogIn.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
 
 const LogIn = () => {
   return (
-    <div>
-      <h1> This is Log in page</h1>
-    </div>
+    <Container>
+      <h1> Log in here to join BIKE DECOR</h1>
+      <div>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" required />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" required />
+          </Form.Group>
+          <Button type="submit">Log in</Button>
+        </Form>
+        <p>
+          New here?
+          <span> Register now</span>
+        </p>
+        <p>
+          Forget Password?
+          <span> Reset Password</span>
+        </p>
+      </div>
+    </Container>
   );
 };
 
