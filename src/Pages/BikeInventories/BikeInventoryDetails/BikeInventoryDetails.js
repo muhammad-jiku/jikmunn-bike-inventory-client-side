5f83e00bb797268909a71ca5d9dfe71b957d56ad
muhammad-jiku <muhammadjiku364@gmail.com>
2022-05-02 18:17:45 +0600

    single inventory data loaded and connected to client side

--- src/Pages/BikeInventories/BikeInventoryDetails/BikeInventoryDetails.js
+++ src/Pages/BikeInventories/BikeInventoryDetails/BikeInventoryDetails.js
@@ -3,20 +3,20 @@ import { useParams } from 'react-router-dom';
 
 const BikeInventoryDetails = () => {
   const { manageinventoryId } = useParams();
-  // const [bikeInventoryDetails, setBikeInventoryDetails] = useState({});
-  // useEffect(() => {
-  //   const url = `http://localhost:3000/manageinventory/${manageinventoryId}`;
-  //   console.log(url);
-  //   fetch(url)
-  //     .then((res) => res.json())
-  //     .then((data) => setBikeInventoryDetails(data))
-  //     .catch((err) => console.log(err));
-  // }, [manageinventoryId]);
+  const [bikeInventoryDetails, setBikeInventoryDetails] = useState({});
+  useEffect(() => {
+    const url = `http://localhost:5000/bikeinventory/${manageinventoryId}`;
+    console.log(url);
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setBikeInventoryDetails(data))
+      .catch((err) => console.log(err));
+  }, [manageinventoryId]);
   return (
     <div>
-      {/* {console.log(bikeInventoryDetails)} */}
+      {console.log(bikeInventoryDetails)}
       <h1>This is bikes details of {manageinventoryId}</h1>
-      {/* <h3>Name: {bikeInventoryDetails?.name}</h3> */}
+      <h3>Name: {bikeInventoryDetails?.name}</h3>
     </div>
   );
 };
