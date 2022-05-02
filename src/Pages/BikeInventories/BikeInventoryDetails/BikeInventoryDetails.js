c0b63d46548e250f4ed4dc00d3cc6383e70d718e
muhammad-jiku <muhammadjiku364@gmail.com>
2022-05-02 20:01:08 +0600

    inventory details section added

--- src/Pages/BikeInventories/BikeInventoryDetails/BikeInventoryDetails.js
+++ src/Pages/BikeInventories/BikeInventoryDetails/BikeInventoryDetails.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 
 const BikeInventoryDetails = () => {
@@ -16,7 +17,17 @@ const BikeInventoryDetails = () => {
     <div>
       {console.log(bikeInventoryDetails)}
       <h1>This is bikes details of {manageinventoryId}</h1>
+      <img src={bikeInventoryDetails?.image} alt={bikeInventoryDetails?.name} />
       <h3>Name: {bikeInventoryDetails?.name}</h3>
+      <h4>Price: {bikeInventoryDetails?.price} </h4>
+      <h4>Quantity: {bikeInventoryDetails?.quantity} </h4>
+      <h4>Supplier: {bikeInventoryDetails?.supplier} </h4>
+      <h5>
+        Owned by <i> {bikeInventoryDetails?.brand}</i>{' '}
+      </h5>
+      <Button>Delivered</Button>
+      <input type="text" placeholder="Add quantity" />
+      <Button>Add Quantity</Button>
     </div>
   );
 };
