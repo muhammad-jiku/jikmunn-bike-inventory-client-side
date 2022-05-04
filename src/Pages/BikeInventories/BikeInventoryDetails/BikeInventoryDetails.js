4a24efd054b2acd91319f39b2db2aa54ab8b2420
muhammad-jiku <muhammadjiku364@gmail.com>
2022-05-05 05:51:51 +0600

    add quantity button added

--- src/Pages/BikeInventories/BikeInventoryDetails/BikeInventoryDetails.js
+++ src/Pages/BikeInventories/BikeInventoryDetails/BikeInventoryDetails.js
@@ -13,6 +13,37 @@ const BikeInventoryDetails = () => {
       .then((data) => setBikeInventoryDetails(data))
       .catch((err) => console.log(err));
   }, [manageinventoryId]);
+
+  const handleUpdateQuantity = (e) => {
+    e.preventDefault();
+
+    const quantity = e.target.quantity.value;
+    // console.log(quantity);
+    if (quantity > 0) {
+      const updatedInventory = { quantity };
+      // console.log(updatedInventory);
+      const url = `http://localhost:5000/bikeinventory/${manageinventoryId}`;
+      // console.log(url);
+      fetch(url, {
+        method: 'PUT',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify(updatedInventory),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          alert('quantity updated successfully');
+          e.target.reset();
+        })
+        .catch((err) => console.log(err));
+    } else {
+      alert('Please insert positive number of quantiy');
+      e.target.reset();
+      return;
+    }
+  };
   return (
     <div>
       {console.log(bikeInventoryDetails)}
@@ -20,14 +51,22 @@ const BikeInventoryDetails = () => {
       <img src={bikeInventoryDetails?.image} alt={bikeInventoryDetails?.name} />
       <h3>Name: {bikeInventoryDetails?.name}</h3>
       <h4>Price: {bikeInventoryDetails?.price} </h4>
-      <h4>Quantity: {bikeInventoryDetails?.quantity} </h4>
+      <h4>quantity: {bikeInventoryDetails?.quantity} </h4>
       <h4>Supplier: {bikeInventoryDetails?.supplier} </h4>
       <h5>
         Owned by <i> {bikeInventoryDetails?.brand}</i>{' '}
       </h5>
       <Button>Delivered</Button>
-      <input type="text" placeholder="Add quantity" />
-      <Button>Add Quantity</Button>
+      <form onSubmit={handleUpdateQuantity}>
+        <input
+          type="text"
+          placeholder="Add quantity"
+          name="quantity"
+          id="quantity"
+          required
+        />
+        <Button type="submit">Add Quantity</Button>
+      </form>
     </div>
   );
 };
